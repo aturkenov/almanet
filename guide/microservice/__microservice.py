2261d9baf66abf498ee05d5f354d650f55d43313
Aidar Turkenov <aqturkenov@gmail.com>
2025-04-11 18:44:20 +0500

    fix example

--- guide/microservice/__microservice.py
+++ guide/microservice/__microservice.py
@@ -5,8 +5,6 @@ from . import _greeting
 
 if __name__ == '__main__':
     almanet.serve(
-        "localhost:4150",
-        services=[
-            _greeting.public.service,
-        ],
+        ["localhost:4150"],
+        _greeting.public.service,
     )
